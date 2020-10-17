b486a9e697cbb6e2553f09d37e227459a6febadd
saipratap99 <122005067@sastra.ac.in>
2020-10-17 11:28:58 +0530

    Created branch experiment-2 and initial commit

---
+++ src/experiment_2/BookTapeInheritance.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package experiment_2;
+
+/**
+ *
+ * @author DELL
+ * 
+ */
+class Publication{
+    private String title;
+    private float price;
+    void getData(){
+        
+    }
+}
+class Book extends Publication{
+    private int pageCount;
+    void getData(){
+        
+    }
+}
+class Tape extends Publication{
+    private float playTime;
+    void getData(){
+        
+    }
+}
+
+public class BookTapeInheritance {
+    
+}
