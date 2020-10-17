c8132c9a0c30719c90d136d4da11c3633723bbf6
saipratap99 <122005067@sastra.ac.in>
2020-10-17 12:52:15 +0530

    Program 1 , publication extends by book and tape are done

--- src/experiment_2/BookTapeInheritance.java
+++ src/experiment_2/BookTapeInheritance.java
@@ -5,31 +5,90 @@
  */
 package experiment_2;
 
+import java.util.*;
+
 /**
  *
  * @author DELL
- * 
+ *
  */
-class Publication{
+class Publication {
+
     private String title;
     private float price;
-    void getData(){
-        
+
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Title: ");
+        title = sc.nextLine();
+        System.out.print("Enter Price: ");
+        price = sc.nextFloat();
+    }
+
+    void putData() {
+        System.out.println("Title : " + title);
+        System.out.println("Price : " + price);
     }
 }
-class Book extends Publication{
+
+class Book extends Publication {
+
     private int pageCount;
-    void getData(){
-        
+
+    @Override
+    void getData() {
+        super.getData();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Page count: ");
+        pageCount = sc.nextInt();
+    }
+
+    @Override
+    void putData() {
+        super.putData();
+        System.out.println("Page Count: " + pageCount);
     }
 }
-class Tape extends Publication{
+
+class Tape extends Publication {
+
     private float playTime;
-    void getData(){
-        
+
+    @Override
+    void getData() {
+        super.getData();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Play time: ");
+        playTime = sc.nextFloat();
+    }
+
+    @Override
+    void putData() {
+        super.putData();
+        System.out.println("Play Time: " + playTime);
     }
 }
 
 public class BookTapeInheritance {
-    
+
+    public static void main(String args[]) {
+        Book[] b;
+        b = new Book[5];
+        Tape[] t;
+        t = new Tape[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*******Enter datails of Publication " + (i+1) +" ********");
+            b[i] = new Book();
+            b[i].getData();
+            t[i] = new Tape();
+            t[i].getData();
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*******   " + (i+1) +"   ********");
+            b[i].putData();
+            t[i].putData();
+        }
+        
+    }
+
 }
